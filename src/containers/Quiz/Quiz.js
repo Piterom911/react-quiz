c60f3e838b48c09032317dcfee96eb14023e6ad2
Roman <piterom911@gmail.com>
2020-09-24 23:06:47 +0600

    047 Компонент активного вопроса

--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -1,11 +1,15 @@
 import React, {Component} from "react";
 import classes from "./Quiz.module.scss"
+import ActiveQuiz from "../../components/ActiveQuiz/ActiveQuiz";
 
 class Quiz extends Component{
     render() {
         return (
             <div className={classes.Quiz}>
-                <h1>Quiz</h1>
+                <div className={classes.Wrapper}>
+                    <h1>Quiz</h1>
+                    <ActiveQuiz/>
+                </div>
             </div>
         )
     }
