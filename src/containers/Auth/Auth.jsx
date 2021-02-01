6f2056e4160cb42f0796cca8f0c37efdd3e0e22a
Roman Dogoda <piterom911@gmail.com>
2021-02-01 16:04:42 +0200

    Creat form and work with colors

--- src/containers/Auth/Auth.jsx
+++ src/containers/Auth/Auth.jsx
@@ -1,11 +1,43 @@
 import React from "react";
 import classes from './Auth.module.scss'
+import Button from "../../components/UI/Button/Button";
 
 export default class Auth extends React.Component{
 
+    registerHandler() {}
+
+    signInHandler() {}
+
+    submitHandler(event) {
+        event.preventDefault();
+    }
+
     render() {
+
         return (
-            <h1>Auth</h1>
+            <div className={classes.Auth}>
+                <div className={'wrapper'}>
+                    <h1>Authorization and Registration</h1>
+                    <form onSubmit={this.submitHandler}>
+                        <p>
+                            <input type="text"/>
+                            <input type="text"/>
+                        </p>
+                        <Button
+                            type={'success'}
+                            onClick={this.signInHandler}
+                        >
+                            Sign in
+                        </Button>
+                        <Button
+                            type={'primary'}
+                            onClick={this.registerHandler}
+                        >
+                            Log in
+                        </Button>
+                    </form>
+                </div>
+            </div>
         );
     }
 }
